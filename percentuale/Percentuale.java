1bb118c12968784359813cd7702392d329051bf8
scamgi <scampini.giacomo@gmail.com>
2025-01-27 21:58:14 +0100

    percentuale

---
+++ percentuale/Percentuale.java
@@ -0,0 +1,7 @@
+public class Percentuale {
+  public static void main(String[] args) {
+    for (int i = 0; i < 6; i++) {
+      System.out.println("Se i = " + i + ", allora y = i % 3 = " + i % 3);
+    }
+  }
+}
